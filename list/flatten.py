f710659d24bf40281092cbbdb3a9f32d5c9bc8bd
Senthil Vaiyapuri <svaiyapu@yahoo.com>
2011-05-03 11:48:16 -0700

    add list

---
+++ list/flatten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Flatten nested lists
+
+def flatten(a, stack=[]):
+    ''' Using accumulator'''
+    for e in a:
+        if isinstance(e, list):
+            flatten(e, stack)
+        else:
+            stack.append(e)
+    return stack
+
+def flatten2(a):
+    ''' In place '''
+    i = 0
+    while i < len(a):
+        while isinstance(a[i], list):
+            if len(a[i]) == 0:
+                a.pop(i)
+                i -= 1
+                break
+            else:
+                a[i:i+1] = a[i]
+        i += 1
+    return a
+
+print flatten([1,2,3,[4,5],6,7,[8,9]])
+print flatten2([1,2,3,[4,5],6,7,[8,9]])
+print flatten2([1,2,3,[[4,5]],6,[],7,[8,9]])
