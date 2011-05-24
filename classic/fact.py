79bbd4fc18c46380f9a113bef43a7ac6d7fd26b5
Senthil Vaiyapuri <svaiyapu@yahoo.com>
2011-05-23 21:23:41 -0700

    iterative methods

--- classic/fact.py
+++ classic/fact.py
@@ -7,6 +7,19 @@ def fact(n):
 
     return n * fact(n-1)
 
+def fact2(n):
+    assert n >= 0
+    if n < 2:
+        return 1
+    
+    res = 1
+    for i in range(1,n+1):
+        res *= i
+
+    return res
+
 if __name__ == '__main__':
     for i in range(10):
         print fact(i)
+    for i in range(10):
+        print fact2(i)
--- classic/fib.py
+++ classic/fib.py
@@ -6,6 +6,14 @@ def fib(n):
 
     return fib(n-1) + fib(n-2)
 
+def fib2(n):
+    a,b = 1,1
+    for i in range(1,n):
+        a,b = b,a+b
+    return a
+
 if __name__ == '__main__':
     for i in range(1,10):
         print fib(i)
+    for i in range(1,10):
+        print fib2(i)
